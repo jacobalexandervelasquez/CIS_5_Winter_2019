f08b784a25aa261672635d3c1dead712c5eb1acb
jacobalexandervelasquez <46359027+jacobalexandervelasquez@users.noreply.github.com>
2019-01-19 12:15:43 -0800

    Add files via upload

---
+++ Lab/Lab Assignment 1/main.cpp
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: jacobalexandervelasquez
+ *
+ * Created on January 8, 2019, 10:37 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+float milBdgt,fedBdgt,mlPrcnt;
+
+milBdgt=7.0e11f;    //Military Budget = 700 Billion   
+
+fedBdgt=4.1e12f;    //Federal Budget  = 4.1 Trillion
+
+
+    return 0;
+}
+
